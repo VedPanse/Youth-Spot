152ace1190b567ee8185bef549ffffbff45c578d
Ved Panse <vedkpanse@gmail.com>
2024-04-14 23:08:54 -0700

    landing page

--- main.py
+++
@@ -1,7 +0,0 @@
-def main() -> None:
-    print('Testing if GitHub works.')
-    print('Works on my side too')
-
-
-if __name__ == "__main__":
-    main()
--- rohan.py
+++
@@ -1,1 +0,0 @@
-print("Uploading files on my end")
